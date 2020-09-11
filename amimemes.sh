5606cf77b2eceb6ebbeddccb8f0c5ccf9233d64e
Jannik Hauptvogel <hauptvogel@amicaldo.de>
2020-09-11 12:53:12 +0200

    Kill audio properly

--- amimemes.sh
+++ amimemes.sh
@@ -12,99 +12,109 @@ play_audio() {
     fi
 }
 
+kill_audio() {
+    command -v afplay &> /dev/null
+
+    if [[ $? -eq 0 ]]; then
+        pkill -9 afplay
+    else
+        pkill -9 mpv
+    fi
+}
+
 case "${1}" in
     egal)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/egal &> /dev/null &
         play_audio /var/amimemes/egal.mp3
         ;;
 
     dasdingistgroß)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/dasdingistgroß &> /dev/null &
         play_audio /var/amimemes/das_ding_ist_groß.mp3
         ;;
 
     woranhatsgelegen)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/woranhatsgelegen &> /dev/null &
         play_audio /var/amimemes/woran_hats_gelegen.mp3
         ;;
 
     innenminister)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/innenminister &> /dev/null &
         play_audio /var/amimemes/innenminister.mp3
         ;;
 
     dascostafastgarnix)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/dascostafastgarnix &> /dev/null &
         play_audio /var/amimemes/das_costa_fast_gar_nix.mp3
         ;;
 
     wochenendesaufen)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/wochenendesaufen &> /dev/null &
         play_audio /var/amimemes/wochenende_saufen.mp3
         ;;
 
     ok)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/ok &> /dev/null &
         play_audio /var/amimemes/ok.mp3
         ;;
 
     einhundimbüro)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/einhundimbuero &> /dev/null &
         play_audio /var/amimemes/ein_hund_im_büro.mp3
         ;;
 
     thatswhatshesaid)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/thatswhatshesaid &> /dev/null &
         play_audio /var/amimemes/thats_what_she_said.mp3
         ;;
 
     dochoh)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/dochoh &> /dev/null &
         play_audio /var/amimemes/doch_oh.mp3
         ;;
 
     xfiles)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/xfiles &> /dev/null &
         play_audio /var/amimemes/xfiles.mp3
         ;;
 
     cool)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/cool &> /dev/null &
         play_audio /var/amimemes/cool.mp3
         ;;
 
     shutdown)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/shutdown &> /dev/null &
         play_audio /var/amimemes/shutdown.mp3
         ;;
 
     adolf)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/adolf &> /dev/null &
         play_audio /var/amimemes/adolf.mp3
         ;;
 
     dumm)
-        pkill -9 mpv
+        kill_audio
         curl -X POST https://memes.amicaldo.net/play/dumm &> /dev/null &
         play_audio /var/amimemes/dumm.mp3
         ;;
 
     stop)
-        pkill -9 mpv
+        kill_audio
         ;;
 
     *)
